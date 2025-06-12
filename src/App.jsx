137b8d29bdba574b40ddad6126fa5cf201ac7dc5
Bramasta <bramastasaka9@gmail.com>
2025-06-12 20:12:22 +0700

    set up item dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// import { useState } from 'react'
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <Header />
+        <Form />
+        <Grocery />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function Header() {
+  return <h1>Catatan Belanjaku</h1>;
+}
+
+function Form() {
+  const [name, setName] = useState("");
+  const [setquantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(setquantity);
+  }
+  const quantity = [...Array(20)].map((x, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select value={setquantity} onChange={(e) => setQuantity(e.target.value)}>{quantity}</select>
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function Grocery() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" checked="true" />
+      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
+}
+function Footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
+  );
+}
