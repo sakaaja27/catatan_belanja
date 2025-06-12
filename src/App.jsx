2b529348bb3c2478a62a6438a5e0976703477feb
Bramasta <bramastasaka9@gmail.com>
2025-06-12 20:52:52 +0700

    lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,13 @@ const groceryItems = [
   },
 ];
 export default function App() {
+  const [items, setItems] = useState(groceryItems)
   return (
     <>
       <div className="app">
         <Header />
         <Form />
-        <Grocery />
+        <Grocery items={items}/>
         <Footer />
       </div>
     </>
@@ -40,13 +41,20 @@ function Header() {
 
 function Form() {
   const [name, setName] = useState("");
-  const [setquantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
+    const newItem = {name, quantity, checked:false, id:Date.now()}
+    if (!name){
+      alert('setidaknya ada barang yang harus diisi')
+      return;
+    } 
+    setName('')
+    setQuantity(1)
     e.preventDefault();
     alert(setquantity);
   }
-  const quantity = [...Array(20)].map((x, i) => (
+  const setquantity = [...Array(20)].map((x, i) => (
     <option value={i + 1} key={i + 1}>
       {i + 1}
     </option>
@@ -55,7 +63,7 @@ function Form() {
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select value={setquantity} onChange={(e) => setQuantity(e.target.value)}>{quantity}</select>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{setquantity}</select>
         <input
           type="text"
           placeholder="nama barang..."
@@ -68,12 +76,12 @@ function Form() {
   );
 }
 
-function Grocery() {
+function Grocery({items}) {
   return (
     <>
       <div className="list">
         <ul>
-          {groceryItems.map((item) => (
+          {items.map((item) => (
             <Item item={item} key={item.id} />
           ))}
         </ul>
@@ -92,7 +100,7 @@ function Grocery() {
 function Item({ item }) {
   return (
     <li key={item.id}>
-      <input type="checkbox" checked="true" />
+      <input type="checkbox" />
       <span style={item.checked ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.name}
       </span>
